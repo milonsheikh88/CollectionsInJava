6842463938dbedef86c5fe1d433a8a57e8fc198c
milon <milonsheikh88@gmail.com>
2022-09-26 12:20:24 +0600

    Collection in Java.

--- src/listExample/arrayList/ArrayListExample.java
+++ src/listExample/arrayList/ArrayListExample.java
@@ -28,6 +28,11 @@ public class ArrayListExample {
         languages2.add("C#");
         languages2.add("JavaScript");
 
+        /** Remove element from index 1 */
+        String strRe = languages2.remove(1);
+        System.out.println("Removed Element: " + strRe);
+        System.out.println("Updated ArrayList: " + languages2);
+
         /**Removes all the elements from the arraylist that are also present in the specified collection.*/
         languages.removeAll(languages2);
         System.out.println("After removeAll  ArrayList: " + languages);
@@ -35,6 +40,5 @@ public class ArrayListExample {
         /** removes all the elements from an arraylist.*/
         languages2.clear();
         System.out.println("After removeAll  ArrayList: " + languages2);
-
     }
 }
